657563426aa05ab2e9312bb1123852307e8c4e3b
Yuzuru Sano <sano@suitosha.co,jp>
2015-12-10 20:57:17 +0900

    コメント追加

--- src/AdditionalUtil/Service/AdditionalUtil.php
+++ src/AdditionalUtil/Service/AdditionalUtil.php
@@ -14,11 +14,20 @@ use Config;
 
 class AdditionalUtil
 {
-
+	//サンプル
 	public function test(){
 		return 'Hello We Are SizenkainoOkite!!';
 	}
 
+	/**
+	 * ページidを元にドメイン付きのリンクを返す
+	 * idが設定されていない場合はトップページのURLを返す
+	 * $secureflgをtrueにすることでsslURLを返す
+	 *
+	 * @param int $id
+	 * @param bool $secureflg
+	 * @return str url
+	 */
 	public function the_permalink($id = 1,$secureflg = false){
 		if(Config::get('concrete.myenv') == 'test'){
 			$nom = BASE_URL;
@@ -33,6 +42,31 @@ class AdditionalUtil
 			return $secure . $p->getCollectionPath();
 		}else{
 			return $nom . $p->getCollectionPath();
+		}
+	}
+
+	/**
+	 * $file_objが指定の$thumbnail_type_nameを持つ際にサムネイルURLを返す。
+	 * サムネイルタイプが存在しない場合はフルサイズ画像を返す
+	 *
+	 * @param object $file_obj
+	 * @param str $thumbnail_type_name
+	 * @return str file url
+	 */
+	public function thumb_src($file_obj,$thumbnail_type_name){
+		if($file_obj) {
+			$handles = array();
+			foreach($file_obj->getThumbnails() as $ff){
+				$handles[] = $ff->getThumbnailTypeVersionObject()->getHandle();
+			}
+
+			if(in_array($thumbnail_type_name, $handles)){
+				$file_src = $file_obj->getThumbnailURL($thumbnail_type_name);
+			}else{
+				$file_src = $file_obj->getThumbnailURL('full');
+			}
+
+			return $file_src;
 		}
 	}
 }
