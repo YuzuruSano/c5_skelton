2af6f301de81081b5c4638500711825ddd541fe4
Yuzuru Sano <sano@suitosha.co,jp>
2016-02-11 10:36:41 +0900

    ページネーションをJavascriptの処理に渡すようのグローバル変数追加 フッターの閉じdivが足りていなくて悲しいのを修正

--- themes/base_theme/elements/header.php
+++ themes/base_theme/elements/header.php
@@ -9,25 +9,27 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
 <!-- [if (gte IE 8)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html lang="ja" class="no-js">
 <!-- <![endif]-->
 <head>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="UTF-8">
-  <meta id="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=5.0,user-scalable=1" name="viewport">
-  <?php
-  Loader::element('header_required');
-  //JS用にログイン状態を判定するフラグをグローバルへ設定
-  $u = new User();
-   if($u->isLoggedIn()) {
-      echo '<script>var is_ccmtoolbarvisible = true;</script>';
-   }else{
-      echo '<script>var is_ccmtoolbarvisible = false;</script>';
-   }
-  ?>
-
-  <!--
-  テーマ内のcss・jsを読み込む ※コメントアウトして使用
-  <link rel="stylesheet" href="<?php echo $this->getThemePath()?>/lib/css/bxslider-4/jquery.bxslider.css">
-  <script src="<?php echo $this->getThemePath()?>/js/base.js"></script>
-  -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="UTF-8">
+	<meta id="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=5.0,user-scalable=1" name="viewport">
+	<?php
+	Loader::element('header_required');
+	//JS用にログイン状態を判定するフラグをグローバルへ設定
+	$u = new User();
+	if($u->isLoggedIn()) {
+		echo '<script>var is_ccmtoolbarvisible = true;</script>';
+	}else{
+		echo '<script>var is_ccmtoolbarvisible = false;</script>';
+	}
+	//ページネーションのトータルページ数を渡すためのグローバル変数
+	//ajaxページネーションとかinfinity scroll用
+	?>
+	<script>var cm_this_pagenation_total = 0;</script>
+	<!--
+	テーマ内のcss・jsを読み込む ※コメントアウトして使用
+	<link rel="stylesheet" href="<?php echo $this->getThemePath()?>/lib/css/bxslider-4/jquery.bxslider.css">
+	<script src="<?php echo $this->getThemePath()?>/js/base.js"></script>
+	-->
 <?php
 if($c->isEditMode()){
   //編集モード時に適用したい処理
@@ -42,7 +44,7 @@ if($u->isLoggedIn()) {
 <body id="top">
 <div class="<?php echo $c->getPageWrapperClass();?>">
   <div id="wrap">
-    <?php
-    $au = Core::make('helper/aUtil');
-    echo $au->test();
-    ?>
+	 <?php
+	 $au = Core::make('helper/aUtil');
+	 echo $au->test();
+	 ?>
